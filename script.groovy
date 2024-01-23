bee330c711b0f2d2ee108a736225a0f244fa6818
Facundo Nogueira <97248784+FNogueira2023@users.noreply.github.com>
2024-01-23 10:49:06 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,8 +1,15 @@
 def buildApp(){
-    echo "building the app..."
-    sh 'mvn clean install'
+    echo 'building the app...'
+    sh 'clean install'
 }
 
+def buildImage(){
+    echo 'building the image...'
+    withCredentials([usernamePassword(credentialsId:"docker_hub", passwordVariable: "PASSWORD", usernameVariable: "USER" )])
+        sh 'docker build -t fnogueira2023/peru_4:1.0 .'
+        sh 'echo $PASSWORD | docker login -u $USER --password-stdin'
+        sh 'docker push fnogueira2023/peru_4:1.0 '
+}
 def deployApp(){
     echo "deploying version ${params.Version }"
 }
